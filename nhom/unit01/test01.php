29790a63a5d8cb2e6558ac439efde960af7d95e4
DinhMinhKhue <dinhminhkhue180219@gmail.com>
2021-03-14 21:50:19 +0700

    commit buoi2

---
+++ nhom/unit01/test01.php
@@ -0,0 +1,12 @@
+<?php 
+
+echo "Đinh Minh Khuê";
+echo "Học nhóm buổi 1."
+echo "Học nhóm buổi 1."
+echo "Học nhóm buổi 1."
+echo "Đinh Minh Khuê";
+echo "Học nhóm buổi 1."
+echo "Học nhóm buổi 1."
+echo "Học nhóm buổi 1."
+
+ ?>
